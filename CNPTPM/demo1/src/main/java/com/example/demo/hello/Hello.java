ad8d69be67b7dbeef45ff96d12681af7577e2639
NguyenThinh1903 <thinhnguyen190304@gmail.com>
2024-12-26 14:42:00 +0700

    up

--- CNPTPM/demo1/src/main/java/com/example/demo/hello/Hello.java
+++ CNPTPM/demo1/src/main/java/com/example/demo/hello/Hello.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
+    private static final String GREETING_MESSAGE = "<h1>Hello</h1>";
+    private static final String SHOP_MESSAGE = "<h2>Shop</h2>";
+
     @GetMapping("/")
     public String hello() {
-        return "<h1>Hello</h1>";
+        return GREETING_MESSAGE;
     }
 
     @GetMapping("/shop")
     public String shop() {
-        return "<h2>Shop</h2>";
+        return SHOP_MESSAGE;
     }
 }
