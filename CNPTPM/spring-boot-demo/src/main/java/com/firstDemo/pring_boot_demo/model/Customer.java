b0fd7d2d645893eb0cf52f27179b5ac99067309b
NguyenThinh1903 <thinhnguyen190304@gmail.com>
2025-02-25 21:49:21 +0700

    up

--- CNPTPM/spring-boot-demo/src/main/java/com/firstDemo/pring_boot_demo/model/Customer.java
+++ CNPTPM/spring-boot-demo/src/main/java/com/firstDemo/pring_boot_demo/model/Customer.java
@@ -1,36 +1,51 @@
 package com.firstDemo.pring_boot_demo.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
     @NotNull(message = "Tên công ty là bắt buộc")
     @Size(min = 1, message = "Tên công ty là bắt buộc")
+    @Column(name = "name_of_company")
     private String nameOfCompany;
 
     @NotNull(message = "Địa chỉ đường là bắt buộc")
     @Size(min = 1, message = "Địa chỉ đường là bắt buộc")
+    @Column(name = "street_address")
     private String streetAddress;
 
     @NotNull(message = "Thành phố là bắt buộc")
     @Size(min = 1, message = "Thành phố là bắt buộc")
-    @Pattern(regexp = "^[a-zA-ZÀ-ỹ\\s]+$", message = "Thành phố không được chứa số.")
+    @Pattern(regexp = "^[a-zA-ZÀ-ỹ\\s]+$", message = "Thành phố không được chứa số")
+    @Column(name = "city")
     private String city;
 
     @NotNull(message = "Quốc gia là bắt buộc")
     @Size(min = 1, message = "Quốc gia là bắt buộc")
+    @Column(name = "country")
     private String country;
 
-    @NotNull(message = "Bắt buộc phải có PostalCode")
-    @Size(min = 1, message = "Bắt buộc phải có PostalCode")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9\\-\\s]+$", message = "PostalCode phải chứa cả chữ cái và số.")
+    @NotNull(message = "Mã bưu điện là bắt buộc")
+    @Size(min = 1, message = "Mã bưu điện là bắt buộc")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9\\-\\s]+$", message = "Mã bưu điện phải chứa cả chữ cái và số")
+    @Column(name = "postal_code")
     private String postalCode;
 
-    @NotNull(message = "Bắt buộc phải có Region")
-    @Size(min = 1, message = "Bắt buộc phải có Region")
-    @Pattern(regexp = "^[a-zA-ZÀ-ỹ\\s]+$", message = "Region không được chứa số.")
+    @NotNull(message = "Vùng là bắt buộc")
+    @Size(min = 1, message = "Vùng là bắt buộc")
+    @Pattern(regexp = "^[a-zA-ZÀ-ỹ\\s]+$", message = "Vùng không được chứa số")
+    @Column(name = "region")
     private String region;
 
-    public Customer(String nameOfCompany, String streetAddress, String city, String region, String postalCode, String country) {
+    public Customer(int id, String nameOfCompany, String streetAddress, String city, String region, String postalCode, String country) {
+        this.id = id;
         this.nameOfCompany = nameOfCompany;
         this.streetAddress = streetAddress;
         this.city = city;
@@ -40,7 +55,6 @@ public class Customer {
     }
 
     public Customer() {
-
     }
 
     public String getNameOfCompany() {
@@ -89,5 +103,13 @@ public class Customer {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 }
