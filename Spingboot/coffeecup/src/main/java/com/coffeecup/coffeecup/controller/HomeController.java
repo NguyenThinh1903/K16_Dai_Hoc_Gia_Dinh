439a5f4b00b41974cf9e40e55906816c3e10b019
NguyenThinh1903 <thinhnguyen190304@gmail.com>
2025-03-25 23:06:36 +0700

    up

---
+++ Spingboot/coffeecup/src/main/java/com/coffeecup/coffeecup/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.coffeecup.coffeecup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // Trả về file home.html
+    }
+}
