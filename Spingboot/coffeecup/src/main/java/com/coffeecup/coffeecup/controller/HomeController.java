5627ddc78c10eb8cdeba2792e160bbeb7c5fab8d
NguyenThinh1903 <thinhnguyen190304@gmail.com>
2025-03-26 10:54:37 +0700

    up

--- Spingboot/coffeecup/src/main/java/com/coffeecup/coffeecup/controller/HomeController.java
+++ Spingboot/coffeecup/src/main/java/com/coffeecup/coffeecup/controller/HomeController.java
@@ -10,4 +10,29 @@ public class HomeController {
     public String home() {
         return "home"; // Trả về file home.html
     }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact"; // Trang Contact
+    }
+
+    @GetMapping("/menu")
+    public String menu() {
+        return "menu"; // Trang Contact
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about"; // Trang Contact
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog"; // Trang Contact
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop"; // Trang Contact
+    }
 }
