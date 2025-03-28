323547074ecde8021c4615cff90738636e8a10b7
NguyenThinh1903 <thinhnguyen190304@gmail.com>
2025-03-28 11:16:13 +0700

    up

---
+++ Spingboot/coffeecup-admin-system/src/main/java/com/coffeecup/coffeecupadminsystem/CoffeecupAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coffeecup.coffeecupadminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeecupAdminSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeecupAdminSystemApplication.class, args);
+	}
+
+}
